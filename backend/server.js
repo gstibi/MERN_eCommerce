e3e0277a5a7f6c8f72b893b6f13245b03c479e8a
gstibi <58399636+gstibi@users.noreply.github.com>
2021-11-22 14:31:11 +0100

    db connection and models

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+import products from './data/products.js'
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -16,4 +23,6 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product)
 })
 
-app.listen(4000, console.log('Server running on port 4000'))
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
